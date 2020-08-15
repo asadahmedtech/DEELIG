b7a598452f0fd3c70c34e25f761bcb25b6ead30a
Asad Ahmed <aa@semut.in>
2020-08-15 11:40:38 +0530

    Fix padel error

--- Database Preprocessing/prepare_protein_data.py
+++ Database Preprocessing/prepare_protein_data.py
@@ -56,6 +56,7 @@ def parse_PSSM(file, path = '/home/binnu/Asad/dataset/pdbbind/pssm/'):
 def calc_features(PATH, pdb_ligand_ID, OUTPATH):
 
 	#Loading the files
+	global errors, naccess_errors
 	parser = PDBParser(PERMISSIVE = 1)
 
 	PDB_id = pdb_ligand_ID[:4].lower() #+ '_pocket'
@@ -90,56 +91,60 @@ def calc_features(PATH, pdb_ligand_ID, OUTPATH):
 	
 	feature = dict() #The feature dictionary
 
-	for model in structure:
-		for chain in model:
-			# Modify Output PDB to compute all the chains in a single complex.
-			pssm_ID = chain.get_full_id()[0][:4].upper() + '_' + str(chain.get_full_id()[1]+1)
-			pssm = parse_PSSM(pssm_ID)
-			if pssm == None:
-				continue
-			start = True
-			gap = 0
-			idx_prev = 0
-			for residue in chain:
-				# if(start):
-					# start_idx =residue.get_full_id()[3][1]
-					# idx_prev = 0
-				idx = residue.get_full_id()[3][1]
-				if(idx < 1):
-					print(idx)
-					a = 0
-					pass
-				elif(idx - idx_prev >= 1):
-					print(idx)
-					a = 1
-					gap += idx - idx_prev -1
-				# elif(start):
-					# gap += -1
-					# start = False
-				
-				
-					for atom in residue:
-						# print(atom.get_full_id())					
-						ID = (atom.get_full_id()[2], atom.get_full_id()[3])
+	try:
+		for model in structure:
+			for chain in model:
+				# Modify Output PDB to compute all the chains in a single complex.
+				pssm_ID = chain.get_full_id()[0][:4].upper() + '_' + str(chain.get_full_id()[1]+1)
+				pssm = parse_PSSM(pssm_ID)
+				if pssm == None:
+					continue
+				start = True
+				gap = 0
+				idx_prev = 0
+				for residue in chain:
+					# if(start):
+						# start_idx =residue.get_full_id()[3][1]
+						# idx_prev = 0
+					idx = residue.get_full_id()[3][1]
+					if(idx < 1):
+						print(idx)
+						a = 0
+						pass
+					elif(idx - idx_prev >= 1):
+						print(idx)
+						a = 1
+						gap += idx - idx_prev -1
+					# elif(start):
+						# gap += -1
+						# start = False
+					
+					
+						for atom in residue:
+							# print(atom.get_full_id())					
+							ID = (atom.get_full_id()[2], atom.get_full_id()[3])
 
-						if(ID in list(dssp.keys())):
-							if(rsa[ID]["all_atoms_abs"] > RSA_Threshold):
-								rsa_label = 1
+							if(ID in list(dssp.keys())):
+								if(rsa[ID]["all_atoms_abs"] > RSA_Threshold):
+									rsa_label = 1
+								else:
+									rsa_label = 0
+
+								print(gap, atom.get_full_id()[3][1], a)
+								feat = (SS_Labels[dssp[ID][2]], dssp[ID][4]/360, dssp[ID][5]/360, rsa_label) + tuple(pssm[str(atom.get_full_id()[3][1] - gap)])
+								feature[tuple(atom.get_coord())] = feat
+
+								print(pdb_ligand_ID[:4], ID, atom.get_coord(), feat)
+								dssp_present += 1
+
 							else:
-								rsa_label = 0
-
-							print(gap, atom.get_full_id()[3][1], a)
-							feat = (SS_Labels[dssp[ID][2]], dssp[ID][4]/360, dssp[ID][5]/360, rsa_label) + tuple(pssm[str(atom.get_full_id()[3][1] - gap)])
-							feature[tuple(atom.get_coord())] = feat
-
-							print(pdb_ligand_ID[:4], ID, atom.get_coord(), feat)
-							dssp_present += 1
-
-						else:
-							print(">>> ID not present : ", atom.get_full_id())
-							dssp_not_present += 1
-					idx_prev = idx
-
+								print(">>> ID not present : ", atom.get_full_id())
+								dssp_not_present += 1
+						idx_prev = idx
+	except Exception as e:
+		print("FILE NOT PRESENT: ", PDB_id)
+		errors.append(PDB_id)
+		return
 	#Printing the Stats 
 	print("===> STATS : PDBID : %s , DSSP PRESENT : %s , DSSP NOT PRESENT : %s"%(PDB_id, dssp_present, dssp_not_present))
 
@@ -150,6 +155,8 @@ def calc_features(PATH, pdb_ligand_ID, OUTPATH):
 
 
 if __name__ == '__main__':
+	global errors, naccess_errors
+
 	input_dir = '/home/binnu/Asad/dataset/pdbbind/protein_pdb/'
 	output_dir = "/home/binnu/Asad/dataset/pdbbind/protein_pdb_featurized/"
 	IDs = '/home/binnu/Asad/dataset/pdbbind/PDBBind.txt'
