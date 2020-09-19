f7fff58c82b216f725a8a17ae42f44e4a6141f8b
Asad Ahmed <aa@semut.in>
2020-09-20 00:43:29 +0530

    dir

--- Database Preprocessing/split_dataset.py
+++ Database Preprocessing/split_dataset.py
@@ -29,21 +29,28 @@ with h5py.File('%s/training_set_pocket.hdf' % args.output_path, 'w') as g_p, \
         h5py.File('%s/data_ligand.hdf' % args.input_path, 'r') as f_l:
         data_shuffled = shuffle(list(f_p.keys()), random_state=123)
         print(len(list(f_l.keys())))
+        f_l_keys = list(f_l.keys())
         for pdb_id in data_shuffled[:args.size_val]:
-            if(pdb_id not in ['1APB_FCB_A_308', '1BIR_2GP_B_107']):
+            if(pdb_id in f_l_keys):
                 ds_p = h_p.create_dataset(pdb_id, data=f_p[pdb_id])
                 ds_p.attrs['affinity'] = f_p[pdb_id].attrs['affinity']
                 ds_l = h_l.create_dataset(pdb_id, data=f_l[pdb_id])
-                ds_l.attrs['affinity'] = f_l[pdb_id].attrs['affinity']
+                ds_l.attrs['affinity'] = f_l[pdb_id].attrs['affinity']]
+            else:
+                print(pdb_id)
         for pdb_id in data_shuffled[args.size_val:args.size_test+args.size_val]:
-            if(pdb_id not in ['1APB_FCB_A_308', '1BIR_2GP_B_107']):
+            if(pdb_id in f_l_keys):
                 ds_p = i_p.create_dataset(pdb_id, data=f_p[pdb_id])
                 ds_p.attrs['affinity'] = f_p[pdb_id].attrs['affinity']
                 ds_l = i_l.create_dataset(pdb_id, data=f_l[pdb_id])
                 ds_l.attrs['affinity'] = f_l[pdb_id].attrs['affinity']
+            else:
+                print(pdb_id)
         for pdb_id in data_shuffled[args.size_test+args.size_val:] :
-            if(pdb_id not in ['1APB_FCB_A_308', '1BIR_2GP_B_107']):
+            if(pdb_id in f_l_keys):
                 ds_p = g_p.create_dataset(pdb_id, data=f_p[pdb_id])
                 ds_p.attrs['affinity'] = f_p[pdb_id].attrs['affinity']
                 ds_l = g_l.create_dataset(pdb_id, data=f_l[pdb_id])
                 ds_l.attrs['affinity'] = f_l[pdb_id].attrs['affinity']
+            else:
+                print(pdb_id)
