bce080654fbafae5da80104a3a20f483aa649cd6
Asad Ahmed <aa@semut.in>
2020-09-21 23:01:05 +0530

    model update

--- torch_train.py
+++ torch_train.py
@@ -170,7 +170,7 @@ print('\n---- DATA ----\n')
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 print('==> Building network..')
-net = net1()
+net = net()
 if torch.cuda.device_count() > 1:
   print("Let's use", torch.cuda.device_count(), "GPUs!")
   # dim = 0 [30, xxx] -> [10, ...], [10, ...], [10, ...] on 3 GPUs
