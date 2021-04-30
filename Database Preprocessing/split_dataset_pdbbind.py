b0c8e0d8bba3b695703bf89d4358e090b710a0a2
Asad Ahmed <ahmed.asadtech@gmail.com>
2021-04-30 12:12:53 +0530

    Update split data

--- Database Preprocessing/split_dataset_pdbbind.py
+++ Database Preprocessing/split_dataset_pdbbind.py
@@ -37,6 +37,9 @@ casf = list(casf)
 print(len(casf))
 
 casfinVal = 0
+casfinTrain = []
+casftoMove = 0
+casfinValtoTrain = []
 # create files with the training and validation sets
 with h5py.File('%s/training_set_pocket.hdf' % args.output_path, 'w') as g_p, \
      h5py.File('%s/validation_set_pocket.hdf' % args.output_path, 'w') as h_p, \
@@ -45,51 +48,60 @@ with h5py.File('%s/training_set_pocket.hdf' % args.output_path, 'w') as g_p, \
      h5py.File('%s/validation_set_ligand.hdf' % args.output_path, 'w') as h_l, \
      h5py.File('%s/test_set_ligand.hdf' % args.output_path, 'w') as i_l:
 
-    with h5py.File('%s/data_pocket.hdf' % args.input_path, 'r') as f_p, \
-        h5py.File('%s/data_ligand.hdf' % args.input_path, 'r') as f_l:
+    with h5py.File('%s/training_set_pocket.hdf' % args.input_path, 'r') as g_p_old, \
+     h5py.File('%s/validation_set_pocket.hdf' % args.input_path, 'r') as h_p_old, \
+     h5py.File('%s/test_set_pocket.hdf' % args.input_path, 'r') as i_p_old, \
+     h5py.File('%s/training_set_ligand.hdf' % args.input_path, 'r') as g_l_old, \
+     h5py.File('%s/validation_set_ligand.hdf' % args.input_path, 'r') as h_l_old, \
+     h5py.File('%s/test_set_ligand.hdf' % args.input_path, 'r') as i_l_old:
         
         #PDBBind Set
         print("PDBBind Before: ", len(list(f_p.keys())), len(list(f_l.keys())), casfinVal)
-        for pdb_id in list(f_p.keys()):
-            if(pdb_id[:4].upper() in casf and pdb_id in list(f_l.keys())):
-                casfinVal += 1
-                ds_p = h_p.create_dataset(pdb_id, data=f_p[pdb_id])
-                ds_p.attrs['affinity'] = f_p[pdb_id].attrs['affinity']
-                ds_l = h_l.create_dataset(pdb_id, data=f_l[pdb_id])
-                ds_l.attrs['affinity'] = f_l[pdb_id].attrs['affinity']
+        for pdb_id in list(g_p_old.keys()):
+            if(pdb_id[:4].upper() in casf):
+                casfinTrain.append(pdb_id)
+        
+        for pdb_id in list(h_p_old.keys()):
+            if(casftoMove>=len(casfinTrain)):
+                break
+            if(pdb_id[:4].upper() not in casf):
+                casfinValtoTrain.append(pdb_id)
+                casftoMove += 1
 
-                del f_p[pdb_id]
-                del f_l[pdb_id]
-            else:
-                print(pdb_id)
+        print("CASF in Train: {}".format(casftoMove))
+        
+        for pdb_id in list(g_p_old.keys()):
+            if(pdb_id not in casfinTrain):
+                ds_p = g_p.create_dataset(pdb_id, data=g_p_old[pdb_id])
+                ds_p.attrs['affinity'] = g_p_old[pdb_id].attrs['affinity']
+                ds_l = g_l.create_dataset(pdb_id, data=g_l_old[pdb_id])
+                ds_l.attrs['affinity'] = g_l_old[pdb_id].attrs['affinity']
+                
+        for pdb_id in list(g_p_old.keys()):
+            if(pdb_id in casfinTrain):
+                ds_p = h_p.create_dataset(pdb_id, data=g_p_old[pdb_id])
+                ds_p.attrs['affinity'] = g_p_old[pdb_id].attrs['affinity']
+                ds_l = h_l.create_dataset(pdb_id, data=g_l_old[pdb_id])
+                ds_l.attrs['affinity'] = g_l_old[pdb_id].attrs['affinity']
 
-        print("PDBBind After: ", len(list(f_p.keys())), len(list(f_l.keys())), casfinVal)
+        for pdb_id in list(h_p_old.keys()):
+            if(pdb_id not in casfinValtoTrain):
+                ds_p = g_p.create_dataset(pdb_id, data=h_p_old[pdb_id])
+                ds_p.attrs['affinity'] = h_p_old[pdb_id].attrs['affinity']
+                ds_l = g_l.create_dataset(pdb_id, data=h_l_old[pdb_id])
+                ds_l.attrs['affinity'] = h_l_old[pdb_id].attrs['affinity']
+                
+        for pdb_id in list(h_p_old.keys()):
+            if(pdb_id in casfinValtoTrain):
+                ds_p = h_p.create_dataset(pdb_id, data=g_p_old[pdb_id])
+                ds_p.attrs['affinity'] = g_p_old[pdb_id].attrs['affinity']
+                ds_l = h_l.create_dataset(pdb_id, data=g_l_old[pdb_id])
+                ds_l.attrs['affinity'] = g_l_old[pdb_id].attrs['affinity']
+        
+        for pdb_id in list(i_p_old.keys()):
+            ds_p = i_p.create_dataset(pdb_id, data=i_p_old[pdb_id])
+            ds_p.attrs['affinity'] = i_p_old[pdb_id].attrs['affinity']
+            ds_l = i_l.create_dataset(pdb_id, data=i_l_old[pdb_id])
+            ds_l.attrs['affinity'] = i_l_old[pdb_id].attrs['affinity']
 
-        data_shuffled = shuffle(list(f_p.keys()), random_state=123)
-        print(len(list(f_l.keys())))
-        f_l_keys = list(f_l.keys())
-
-        for pdb_id in data_shuffled[:args.size_val-casfinVal]:
-            if(pdb_id in f_l_keys):
-                ds_p = h_p.create_dataset(pdb_id, data=f_p[pdb_id])
-                ds_p.attrs['affinity'] = f_p[pdb_id].attrs['affinity']
-                ds_l = h_l.create_dataset(pdb_id, data=f_l[pdb_id])
-                ds_l.attrs['affinity'] = f_l[pdb_id].attrs['affinity']
-            else:
-                print(pdb_id)
-        for pdb_id in data_shuffled[args.size_val-casfinVal:args.size_test+args.size_val-casfinVal]:
-            if(pdb_id in f_l_keys):
-                ds_p = i_p.create_dataset(pdb_id, data=f_p[pdb_id])
-                ds_p.attrs['affinity'] = f_p[pdb_id].attrs['affinity']
-                ds_l = i_l.create_dataset(pdb_id, data=f_l[pdb_id])
-                ds_l.attrs['affinity'] = f_l[pdb_id].attrs['affinity']
-            else:
-                print(pdb_id)
-        for pdb_id in data_shuffled[args.size_test+args.size_val-casfinVal:] :
-            if(pdb_id in f_l_keys):
-                ds_p = g_p.create_dataset(pdb_id, data=f_p[pdb_id])
-                ds_p.attrs['affinity'] = f_p[pdb_id].attrs['affinity']
-                ds_l = g_l.create_dataset(pdb_id, data=f_l[pdb_id])
-                ds_l.attrs['affinity'] = f_l[pdb_id].attrs['affinity']
-            else:
-                print(pdb_id)
+        print("Train : {} :: Test : {} :: Val : {} ".format(len(list(g_p.keys())), len(list(h_p.keys())), len(list(i_p.keys()))))
